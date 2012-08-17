70725803672f5eca31d163fae2e582dc9313dbc0
Bill Yang <zz_fish@hotmail.com>
2012-08-17 10:14:29 -0600

    552

---
+++ SRM/552.Round1/250/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _250
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test0();
+            Test2();
+            Test3();
+            Test4();
+
+            Console.ReadLine();
+        }
+
+        static void Test0()
+        {
+            var flower = new string[] {
+                             "F.F",
+                             ".F.",
+                             ".F."
+                         };
+            var r = 1;
+            var c = 1;
+
+            var shop = new FoxAndFlowerShopDivTwo();
+            Console.WriteLine(shop.theMaxFlowers(flower, r, c));
+        }
+
+        static void Test2()
+        {
+            var shop = new FoxAndFlowerShopDivTwo();
+            Console.WriteLine(shop.theMaxFlowers(new string[]
+                                                     {
+                                                         ".FF.F.F",
+                                                         "FF...F.",
+                                                         "..FF.FF"
+                                                     }, 1, 2));
+        }
+
+        static void Test3()
+        {
+
+            var shop = new FoxAndFlowerShopDivTwo();
+            Console.WriteLine(shop.theMaxFlowers(new string[]
+                                                     {
+                                                         "F",
+                                                         ".",
+                                                         "F",
+                                                         "F",
+                                                         "F",
+                                                         ".",
+                                                         "F",
+                                                         "F"
+                                                     }, 4, 0));
+        }
+
+        static void Test4()
+        {
+            var shop = new FoxAndFlowerShopDivTwo();
+            Console.WriteLine(shop.theMaxFlowers(new string[]
+                                                     {
+                                                         ".FFF..F...",
+                                                         "FFF...FF.F",
+                                                         "..F.F.F.FF",
+                                                         "FF..F.FFF.",
+                                                         "..FFFFF...",
+                                                         "F....FF...",
+                                                         ".FF.FF..FF",
+                                                         "..F.F.FFF.",
+                                                         ".FF..F.F.F",
+                                                         "F.FFF.FF.F"
+                                                     }, 4, 3));
+        }
+    }
+}
---
+++ SRM/552.Round1/500/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test0();
+            Test1();
+            Test2();
+            Test3();
+            Test4();
+            Test5();
+            Test6();
+
+            Test7();
+
+            Test8();
+
+            Console.ReadLine();
+        }
+
+        static void Test8()
+        {
+            var fox = new FoxPaintingBalls();
+            Console.WriteLine(fox.theMax(1000000000000000000, 1000000000000000000, 1000000000000000000, 4));
+            
+        }
+
+        static void Test7()
+        {
+            var fox = new FoxPaintingBalls();
+            Console.WriteLine(fox.theMax(12, 12, 9, 4));
+        }
+
+        static void Test0()
+        {
+            var fox = new FoxPaintingBalls();
+            Console.WriteLine(fox.theMax(2, 2, 2, 3));
+        }
+
+        static void Test1()
+        {
+            var fox = new FoxPaintingBalls();
+            Console.WriteLine(fox.theMax(1, 2, 3, 3));
+        }
+
+        static void Test2()
+        {
+            var fox = new FoxPaintingBalls();
+            Console.WriteLine(fox.theMax(8, 6, 6, 4));
+        }
+
+        static void Test3()
+        {
+            var fox = new FoxPaintingBalls();
+            Console.WriteLine(fox.theMax(7, 6, 7, 4));
+        }
+
+        static void Test4()
+        {
+            var fox = new FoxPaintingBalls();
+            Console.WriteLine(fox.theMax(100, 100, 100, 4));
+        }
+
+        static void Test5()
+        {
+            var fox = new FoxPaintingBalls();
+            Console.WriteLine(fox.theMax(19330428391852493, 48815737582834113, 11451481019198930, 3456));
+        }
+
+
+        static void Test6()
+        {
+            var fox = new FoxPaintingBalls();
+            Console.WriteLine(fox.theMax(1, 1, 1, 1));
+        }
+    }
+}
