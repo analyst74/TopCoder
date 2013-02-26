3ad153cdffe6c5cb89bb441f2c336fa2559e67c2
Bill Yang <zz_fish@hotmail.com>
2013-02-25 23:15:02 -0700

    551.1

---
+++ SRM_practice/551.1/250/ColorfulChocolates.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250
+{
+    public class ColorfulChocolates
+    {
+        Dictionary<string, Tuple<int, int>> hash;
+        public ColorfulChocolates()
+        {
+            hash = new Dictionary<string,Tuple<int,int>>();
+        }
+
+        public int maximumSpread(string chocolates, int maxSwap)
+        {
+            if (hash.ContainsKey(chocolates) && hash[chocolates].Item1 >= maxSwap)
+                return hash[chocolates].Item2;
+
+
+            var maxSpread = calcSpread(chocolates);
+
+            if (maxSwap > 0)
+            {
+                for (int i = 0; i < chocolates.Length - 1; i++)
+                {
+                    var sequence = chocolates.ToCharArray();
+                    var temp = sequence[i];
+                    sequence[i] = sequence[i + 1];
+                    sequence[i + 1] = temp;
+                    var spread = maximumSpread(new string(sequence), maxSwap - 1);
+                    if (spread > maxSpread)
+                        maxSpread = spread;
+                }
+            }
+
+            hash[chocolates] = new Tuple<int, int>(maxSwap, maxSpread);
+            return maxSpread;
+        }
+
+        public int calcSpread(string chocolates)
+        {
+            var prev = ' ';
+            var counter = 0;
+            var max = 0;
+            for (int i = 0; i < chocolates.Length; i++)
+            {
+                if (prev == chocolates[i])
+                {
+                    counter++;
+                    if (max < counter)
+                        max = counter;
+                }
+                else
+                {
+                    prev = chocolates[i];
+                    counter = 0;
+                }
+            }
+
+            return max+1;
+        }
+    }
+}
---
+++ SRM_practice/551.1/250/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cc = new ColorfulChocolates();
+
+            Console.WriteLine("ABCDCBC, 1 = " + cc.maximumSpread("ABCDCBC", 1));
+            Console.WriteLine("ABCDCBC, 2 = " + cc.maximumSpread("ABCDCBC", 2));
+            Console.WriteLine("ABBABABBA, 3 = " + cc.maximumSpread("ABBABABBA", 3));
+            Console.WriteLine("ABBABABBA, 4 = " + cc.maximumSpread("ABBABABBA", 4));
+            Console.WriteLine("QASOKZNHWNFODOQNHGQKGLIHTPJUVGKLHFZTGPDCEKSJYIWFOO, 77 = " + cc.maximumSpread("QASOKZNHWNFODOQNHGQKGLIHTPJUVGKLHFZTGPDCEKSJYIWFOO", 77));
+            Console.ReadLine();
+        }
+
+
+    }
+}
